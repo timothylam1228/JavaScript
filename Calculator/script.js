5f81d4e9cf8816442e787a0833095470e7e8af0e
BuildTools <unconfigured@null.spigotmc.org>
2020-03-05 12:08:37 +0800

    finish

--- Calculator/script.js
+++ Calculator/script.js
@@ -7,6 +7,10 @@ function equal(){
         document.form.textview.value = eval(exp)
     }
 }
-function clear(){
-    document.form.textview.value=''
+function c(){
+    document.form.textview.value="";
+}
+function back(){
+    var exp = document.form.textview.value;
+    document.form.textview.value = exp.substring(0,exp.length-1);
 }
