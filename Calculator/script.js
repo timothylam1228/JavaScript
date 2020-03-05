2c4440b0541be279eb483d2728d521c1e6908495
leunglong0123 <58369792+leunglong0123@users.noreply.github.com>
2020-03-06 03:19:26 +0800

    Calculator

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,5 +1,5 @@
 function insert(num){
-    document.form.textview.value = document.form.textview.value+num
+    document.form.textview.value = document.form.textview.value+num;
 }
 function equal(){
     var exp = document.form.textview.value;
@@ -10,7 +10,27 @@ function equal(){
 function c(){
     document.form.textview.value="";
 }
+function divideMath(){
+    insert('/');
+}
 function back(){
     var exp = document.form.textview.value;
     document.form.textview.value = exp.substring(0,exp.length-1);
+}
+var Trigo = false;
+function piMath(){
+    document.form.textview.value = document.form.textview.value+Math.PI;
+}
+var i = 0;
+function TrigMath(syn){
+    insert(syn+'(');
+    i++;
+}
+function parentheses(syn){
+    insert(syn);
+}
+function square(){
+    var long = document.form.textview.value.length;
+    insert('*');
+    insert(document.form.textview.value.slice(long-1,long));
 }
